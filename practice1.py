e2f83a0219795afe76531289fd9be0b1c180e705
Clark Williams <CWillia5@1800contacts.com>
2022-03-31 17:46:28 -0600

    Calculate the Profit

--- practice1.py
+++ practice1.py
@@ -159,3 +159,56 @@ def area_of_country(name, area):
 print(area_of_country("Russia", 17098242)) # "Russia is 11.48% of the total world's landmass"
 print(area_of_country("USA", 9372610)) # "USA is 6.29% of the total world's landmass"
 print(area_of_country("Iran", 1648195)) # "Iran is 1.11% of the total world's landmass"
+
+"""
+Calculate the Profit
+You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product. You are given a dictionary containing the cost price per unit (in dollars), sell price per unit (in dollars), and the starting inventory. Return the total profit made, rounded to the nearest dollar.
+
+Examples
+profit({
+  "cost_price": 32.67,
+  "sell_price": 45.00,
+  "inventory": 1200
+}) ➞ 14796
+
+profit({
+  "cost_price": 225.89,
+  "sell_price": 550.00,
+  "inventory": 100
+}) ➞ 32411
+
+profit({
+  "cost_price": 2.77,
+  "sell_price": 7.95,
+  "inventory": 8500
+}) ➞ 44030
+Notes
+Assume all inventory has been sold.
+Profit = Total Sales - Total Cost
+"""
+
+def profit(info):
+	cost = info["cost_price"] * info["inventory"]
+	# print(cost)
+	gross = info["sell_price"] * info["inventory"]
+	# print(gross)
+	return gross - cost
+	# return (info["sell_price"] * info["inventory"]) - (info["cost_price"] * info["inventory"])
+
+print(profit({
+  "cost_price": 32.67,
+  "sell_price": 45.00,
+  "inventory": 1200
+})) # 14796
+
+print(profit({
+  "cost_price": 225.89,
+  "sell_price": 550.00,
+  "inventory": 100
+})) # 32411
+
+print(profit({
+  "cost_price": 2.77,
+  "sell_price": 7.95,
+  "inventory": 8500
+})) # 44030
